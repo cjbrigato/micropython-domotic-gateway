9ccc19c48ecc0ec7583d6f4dac7b4575c6e9535a
Colin BRIGATO <colin.brigato@magic.fr>
2017-01-07 17:53:48 +0100

    peping8

--- main.py
+++ main.py
@@ -2,25 +2,23 @@ import network
 import socket
 import machine
 
-from ssd1306 import SSD1306_I2C
+import source_code_pro_ttf_11
+from ssd1306 import SSD1306_I2C, Writer
 WIDTH = const(64)
 HEIGHT = const(48)
 pscl = machine.Pin(5)  # GPIO4_CLK , machine.Pin.OUT_PP)
 psda = machine.Pin(4)  # GPIO5_DAT, machine.Pin.OUT_PP)
 i2c = machine.I2C(scl=pscl, sda=psda)
 ssd = SSD1306_I2C(WIDTH, HEIGHT, i2c)
-
-from writer import Writer
-# Fonts
-import scpl11
-writer = Writer(ssd, scpl11)
+writer = Writer(ssd, source_code_pro_ttf_11)
 
 #[i for i, v in enumerate(network.WLAN(network.STA_IF).scan()) if v[0] == 'excellency']
 
 relay1off = 'http://control.maison.apoui.net/setrelay/1/off'
 relay1on = 'http://control.maison.apoui.net/setrelay/1/on'
 
-def do_connect(ssid,key):
+
+def do_connect(ssid, key):
     sta_if = network.WLAN(network.STA_IF)
     if not sta_if.isconnected():
         writer.printstring('Wifi..')
@@ -33,6 +31,7 @@ def do_connect(ssid,key):
     ssd.show()
     writer.printstring('Ready\n')
     ssd.show()
+
 
 def http_get(url):
     _, _, host, path = url.split('/', 3)
@@ -47,15 +46,17 @@ def http_get(url):
         else:
             break
 
-relayurl='http://control.maison.apoui.net/setrelay'
+relayurl = 'http://control.maison.apoui.net/setrelay'
+
 
 def relay_on(relay):
-	url='{0}/on/{1}'.format(relayurl,relay)
-	http_get(url)
+    url = '{0}/on/{1}'.format(relayurl, relay)
+    http_get(url)
+
 
 def relay_off(relay):
-	url='{0}/off/{1}'.format(relayurl,relay)
-	http_get(url)
+    url = '{0}/off/{1}'.format(relayurl, relay)
+    http_get(url)
 
 Writer.set_clip(True, True)
 writer.printstring('Booting..OK\n')
